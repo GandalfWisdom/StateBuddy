1d62f78cceaf15828fd2729f2c6a3a1c488867d2
GandalfWisdom <jstclair3712@gmail.com>
2025-05-21 17:22:06 -0600

    fixes

--- src/Shared/StateBuddy.lua
+++ src/Shared/StateBuddy.lua
@@ -1,21 +1,112 @@
-local require = require(script.Parent.loader).load(script);
-local Maid = require("Maid");
+--!strict
+local require: any = require(script.Parent.loader).load(script) :: any;
+local Maid: any = require("Maid");
 
 local StateBuddy = {};
 StateBuddy.__index = StateBuddy;
 StateBuddy.ClassName = "StateBuddy";
 
-function StateBuddy.new()
-    local self = {};
+--[=[
+    A state machine utility class that handles transitions between named states.
+
+    @prop _maid Maid -- Used to manage cleanup.
+    @prop _states table<string, StateDefinition> -- All defined states, keyed by name.
+    @prop _current_state string? -- The name of the current active state.
+    @prop _duration number -- Duration tracker for the current state (if applicable).
+
+]=]
+
+export type StateCallback = (...any) -> string? | boolean?;
+export type State = { Name: string, Duration: number?, Enter: StateCallback?, Started: StateCallback?, Completed: StateCallback? };
+export type StateBuddy = typeof(setmetatable(
+    {} :: {
+        _maid: Maid.Maid,
+        _states: { [string]: State },
+        _current_state: string?,
+        _duration: number?,
+        _start_time: number,
+    },
+   {} :: typeof({ __index = StateBuddy })
+));
+
+--[=[
+    Constructs a new StateBuddy object
+    @return StateBuddy
+]=]
+function StateBuddy.new(): StateBuddy
+    local self: StateBuddy = setmetatable({} :: any, StateBuddy);
     self._maid = Maid.new();
+    self._states = {};
+    self._current_state = nil;
+    self._duration = 0;
 
-
-    return setmetatable(self, StateBuddy);
+    return self;
 end;
 
-function StateBuddy:Destroy()
+--[=[
+    Creates a new state.
+    @param state_name string -- Name of the state to be added.
+    @param duration number -- The time the state will last. Set this to 0 to last indefinitely.
+    @param enter StateCallback -- The function that checks if state can be entered into.
+    @param started StateCallback -- The function that runs when state is successfully entered.
+    @param completed StateCallback -- The function that runs when state ends. If duration is set, make sure this function returns a string value of the next state.
+]=]
+function StateBuddy.AddState(self: StateBuddy, state_name: string, duration: number, enter: StateCallback?, started: StateCallback?, completed: StateCallback?): ()
+    self._states[state_name] = {
+        Name = state_name;
+        Duration = duration;
+        Enter = enter;
+        Started = started;
+        Completed = completed;
+    };
+end;
+
+--[=[
+    Changes state.
+    @param state string -- State to change in to.
+]=]
+function StateBuddy.ChangeState(self: StateBuddy, new_state: string): ()
+    if (self._current_state == new_state) then return; end; --Guard Clause. Returns if attempting to change to state that the state machine is already in.
+
+    local next_state: State = self._states[new_state];
+    if not (next_state) then return; end; --Guard Clause. Returns if next_state does not exist.
+
+    local can_enter: boolean? | string? = false;
+    if (next_state.Enter) then can_enter = next_state.Enter(); end;
+    if not (can_enter) then return; end; --Enter guard clause.
+
+    local old_state: State? = self._states[self._current_state or ""];
+    if (old_state) and (old_state.Completed) then old_state.Completed() end; --Run completed function if it exists.
+
+    self._current_state = new_state;
+    self._duration = next_state.Duration;
+    self._start_time = workspace:GetServerTimeNow();
+    if (next_state.Started) then next_state.Started(); end;
+end;
+
+--[=[
+    Updates state machine. Used only when state has a duration above 0.
+]=]
+function StateBuddy.Update(self: StateBuddy): ()
+    if not (self._current_state) then return; end;
+    local state_current: State = self._states[self._current_state];
+    if (state_current.Duration == 0) or (state_current.Duration == nil) then return; end; --Guard clause. If duration is 0 or nil, return.
+
+    local time_now: number = workspace:GetServerTimeNow();
+    if (time_now - self._start_time >= state_current.Duration) then
+        assert(state_current.Completed, "No completed function present! Make sure functions with a duration value have a completed function!");
+        assert(typeof(state_current.Completed()) == "string", "Returned value from .Completed() function is not a string!");
+        
+        self:ChangeState(state_current.Completed() :: string);
+    end;
+end;
+
+--[=[
+    Cleans up the class object and sets the metatable to nil
+]=]
+function StateBuddy.Destroy(self: StateBuddy): ()
     self._maid:DoCleaning();
-    setmetatable(self, nil);
+    setmetatable(self :: any, nil);
 end;
 
 return StateBuddy;
